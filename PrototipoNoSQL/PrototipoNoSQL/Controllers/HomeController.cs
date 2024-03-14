9ea1bf335e9b2d99d0656e96204d9cafb119ab07
Jose Pablo Flores <84046161+jp09flores@users.noreply.github.com>
2024-03-14 16:27:31 -0600

    inicio

---
+++ PrototipoNoSQL/PrototipoNoSQL/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrototipoNoSQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult IniciarSesion()
+        { 
+            return View();
+        }
+        public ActionResult Clientes()
+        {
+            
+
+            return View();
+        }
+        public ActionResult Rol()
+        {
+
+
+            return View();
+        }
+        public ActionResult Empleados()
+        {
+
+
+            return View();
+        }
+        public ActionResult Membresia()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Inventario()
+        {
+
+
+            return View();
+        }
+        public ActionResult Maquinas()
+        {
+
+
+            return View();
+        }
+        public ActionResult Proveedores()
+        {
+
+
+            return View();
+        }
+        public ActionResult Nuevo()
+        {
+
+
+            return View();
+        }
+
+    }
+}
---
+++ PrototipoNoSQL/PrototipoNoSQL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrototipoNoSQL.MvcApplication" Language="C#" %>
